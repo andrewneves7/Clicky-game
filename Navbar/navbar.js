3b3901423df48fa65c8e4068187da5b45bb05ab5
andrewneves7 <42624686+andrewneves7@users.noreply.github.com>
2019-03-08 15:32:41 -0500

    Create navbar.js
    navbar.js

---
+++ Navbar/navbar.js
@@ -0,0 +1,24 @@
+// creating my initial nav bar content (mostly taken from class example and chatting with other classmates)
+import React, { Component } from "react";
+import "./navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+    <nav className="navbar">
+            <div className="navbar-header">
+            <a className="navbar-brand" href='#'><h3>Clicky Game</h3></a>
+            </div>
+        <ul className="nav navbar-nav">
+            <li className="nav-item score">Current Score: {this.props.score}</li>
+            <li className="nav-item wins">Wins: {this.props.gamesWon}</li>
+            <li className="nav-item losses">Losses: {this.props.gamesLost}</li>
+        </ul>
+    </nav>
+);
+}
+}
+
+export default Navbar
+
+
